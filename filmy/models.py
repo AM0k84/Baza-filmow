bb7bc9180a3ad5c0a5befbb136c75a377211f077
Lukasz Chalinski <l.chalinski@gmail.com>
2020-12-27 12:57:01 +0100

    update film model

--- filmy/models.py
+++ filmy/models.py
@@ -6,7 +6,14 @@ from django.db import models
 class Film(models.Model):
     tytul = models.CharField(max_length=80, blank=False, unique=True)
     rok = models.PositiveSmallIntegerField(blank=True, null=True)
+    opis = models.TextField(default="")
+    premiera = models.DateField(blank=True, null=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2,
+                                      blank=True, null=True)
+    plakat = models.ImageField(upload_to="plakaty", null=True, blank=True)
 
     def __str__(self):
-        return f"{self.tytul} ({self.rok})"
+        return self.tytul_z_rokiem()
 
+    def tytul_z_rokiem(self):
+        return "{} ({})".format(self.tytul, self.rok)
