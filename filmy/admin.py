960f566578666366c51dafc331a7403f0d09642e
Lukasz Chalinski <l.chalinski@gmail.com>
2020-12-27 13:16:27 +0100

    register film model in admin

--- filmy/admin.py
+++ filmy/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from filmy.models import Film
 
-admin.site.register(Film)
+# admin.site.register(Film)
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    # fields = ["tytul", "opis", "rok"]
+    # exclude = ["opis"]
+    list_display = ["tytul", "rok", "imdb_rating"]
+    list_filter = ("rok", "imdb_rating")
+    search_fields = ("tytul", "opis",)
