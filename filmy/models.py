d0ae09b91005531f6347aa247cb958d49c7d558b
Lukasz Chalinski <l.chalinski@gmail.com>
2020-12-27 10:49:27 +0100

    first model

--- filmy/models.py
+++ filmy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Film(models.Model):
+    tytul = models.CharField(max_length=80, blank=False, unique=True)
+    rok = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.tytul} ({self.rok})"
+
