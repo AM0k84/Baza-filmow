7a9c4335f1a332ca70003342ba114022f5ac96ed
Lukasz Chalinski <l.chalinski@gmail.com>
2020-12-27 13:57:42 +0100

    first dynamic template

--- filmy/views.py
+++ filmy/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Film
+
 
 # Create your views here.
+
+def wszystkie_filmy(request):
+    # return HttpResponse("<h1>To jest test.</h1>")
+    # test = "to jest coś we views"
+    # return render(request, 'filmy/filmy.html', {'text': test})
+    wszystkie = Film.objects.all()
+    return render(request, 'filmy/filmy.html', {'filmy': wszystkie})
+
+
+
